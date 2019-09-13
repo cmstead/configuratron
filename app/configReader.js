2b7a98c2b7e111cb0db75c58965e1058317976d6
Chris Stead <cmstead@gmail.com>
2019-09-13 15:39:25 -0500

    [Refactor] Extracted method to just do file searching

--- app/configReader.js
+++ app/configReader.js
@@ -1,18 +1,24 @@
-function configReader (fs) {
-    
-    function read(filePaths) {
+function configReader(fs) {
+
+    function getConfigurationString(filePaths) {
         let configurationString = null;
         let filePathsCopy = filePaths.slice(0);
 
-        while(configurationString === null && filePathsCopy.length > 0) {
+        while (configurationString === null && filePathsCopy.length > 0) {
             const filePath = filePathsCopy.shift();
 
-            try{
+            try {
                 configurationString = fs.readFileSync(filePath, { encoding: 'utf8' });
             } catch (e) {
                 // continue
             }
         }
+
+        return configurationString;
+    }
+
+    function read(filePaths) {
+        let configurationString = getConfigurationString(filePaths);
 
         return JSON.parse(configurationString);
     }
